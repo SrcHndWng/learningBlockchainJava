20960f50ca9d7d0664a33e9384f45f3df98d0b98
UnderSourceCode <undersourcecode@hotmail.co.jp>
2018-09-06 21:23:50 +0900

    miningのロジックを実装。

--- src/main/java/com/example/learningBlockchainJava/Application.java
+++ src/main/java/com/example/learningBlockchainJava/Application.java
@@ -24,12 +24,24 @@ public class Application {
 
         ArrayList<Block> blockchain = new ArrayList<Block>();
         final String firstHash = "0";
+        final int difficulty = 5;
 
         blockchain.add(new Block("Hi im the first block", firstHash));
+        System.out.println("Trying to Mine block 1... ");
+        blockchain.get(0).mineBlock(difficulty);
+
         blockchain.add(new Block("Yo im the second block", getPreviousHash(blockchain)));
+        System.out.println("Trying to Mine block 2... ");
+        blockchain.get(1).mineBlock(difficulty);
+
         blockchain.add(new Block("Hey im the third block", getPreviousHash(blockchain)));
+        System.out.println("Trying to Mine block 3... ");
+        blockchain.get(2).mineBlock(difficulty);
+
+        System.out.println("\nBlockchain is Valid: " + isChainValid(blockchain));
 
         String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println("\nThe block chain: ");
         System.out.println(blockchainJson);
 
         System.out.println("learningBlockchainJava end...");
@@ -37,5 +49,27 @@ public class Application {
 
     private String getPreviousHash(ArrayList<Block> blockchain) {
         return blockchain.get(blockchain.size() - 1).hash;
+    }
+
+    private Boolean isChainValid(ArrayList<Block> blockchain) {
+        Block currentBlock;
+        Block previousBlock;
+
+        // loop through blockchain to check hashes:
+        for (int i = 1; i < blockchain.size(); i++) {
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i - 1);
+            // compare registered hash and calculated hash:
+            if (!currentBlock.hash.equals(currentBlock.calculateHash())) {
+                System.out.println("Current Hashes not equal");
+                return false;
+            }
+            // compare previous hash and registered previous hash
+            if (!previousBlock.hash.equals(currentBlock.previousHash)) {
+                System.out.println("Previous Hashes not equal");
+                return false;
+            }
+        }
+        return true;
     }
 }
