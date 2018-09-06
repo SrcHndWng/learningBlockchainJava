e44beee0252978bcb9a4c4b8f243ca22ac4d8cda
UnderSourceCode <undersourcecode@hotmail.co.jp>
2018-09-06 18:21:23 +0900

    初歩的なチェーンを実装。

---
+++ README.md
@@ -0,0 +1,7 @@
+# learningBlockchainJava
+
+## About This
+
+https://medium.com/programmers-blockchain/6eeed3cb03fa
+
+こちらを学習のために写経したもの。
--- src/main/java/com/example/learningBlockchainJava/Application.java
+++ src/main/java/com/example/learningBlockchainJava/Application.java
@@ -1,5 +1,7 @@
 package com.example.learningBlockchainJava;
 
+import com.google.gson.GsonBuilder;
+import java.util.ArrayList;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -7,17 +9,33 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		try (ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args)) {
+    public static void main(String[] args) {
+        try (ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args)) {
             Application app = ctx.getBean(Application.class);
             app.run();
         } catch (Exception e) {
             System.out.println("Application Error!");
             e.printStackTrace();
         }
-	}
+    }
 
-	private void run() throws Exception {
-		System.out.println("learningBlockchainJava run!");
-	}
+    private void run() throws Exception {
+        System.out.println("learningBlockchainJava start...");
+
+        ArrayList<Block> blockchain = new ArrayList<Block>();
+        final String firstHash = "0";
+
+        blockchain.add(new Block("Hi im the first block", firstHash));
+        blockchain.add(new Block("Yo im the second block", getPreviousHash(blockchain)));
+        blockchain.add(new Block("Hey im the third block", getPreviousHash(blockchain)));
+
+        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println(blockchainJson);
+
+        System.out.println("learningBlockchainJava end...");
+    }
+
+    private String getPreviousHash(ArrayList<Block> blockchain) {
+        return blockchain.get(blockchain.size() - 1).hash;
+    }
 }
