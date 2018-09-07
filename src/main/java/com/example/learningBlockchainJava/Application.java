b1660f59248cf1a195112b99f6cebe816b7d7faa
UnderSourceCode <undersourcecode@hotmail.co.jp>
2018-09-07 17:26:04 +0900

    Blockchainとvalidateを実装。

--- src/main/java/com/example/learningBlockchainJava/Application.java
+++ src/main/java/com/example/learningBlockchainJava/Application.java
@@ -1,10 +1,8 @@
 package com.example.learningBlockchainJava;
 
+import com.google.gson.GsonBuilder;
 import java.util.ArrayList;
 import java.util.List;
-
-import com.google.gson.GsonBuilder;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -30,14 +28,33 @@ public class Application {
 
         Block block1 = new Block("First Block", firstHash);
         blockChain.add(block1);
+        System.out.println(String.format("isBlockChainValid = %b", isBlockChainValid(blockChain)));
+
         Block block2 = new Block("Second Block", block1.currentHash);
         blockChain.add(block2);
+        System.out.println(String.format("isBlockChainValid = %b", isBlockChainValid(blockChain)));
+
         Block block3 = new Block("Third Block", block2.currentHash);
         blockChain.add(block3);
+        System.out.println(String.format("isBlockChainValid = %b", isBlockChainValid(blockChain)));
 
         String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockChain);
         System.out.println(blockchainJson);
 
         System.out.println("learningBlockchainJava end...");
+    }
+
+    private static boolean isBlockChainValid(List<Block> blockChain) {
+        if (blockChain.size() < 1) return true;
+
+        for (int i = 1; i <= blockChain.size() - 1; i++) {
+            Block currentBlock = blockChain.get(i - 1);
+            Block nextBlock = blockChain.get(i);
+            if (!(nextBlock.previousHash.equals(currentBlock.currentHash))) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
